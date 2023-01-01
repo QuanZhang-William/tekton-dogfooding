ad0277165090752fe27fca4227028277af4b9df1
Quan Zhang <zhangquan@google.com>
2023-01-01 16:49:59 -0500

    add trigger template for build test

--- github-checks/test/check-script.sh
+++ github-checks/test/check-script.sh
@@ -1,7 +1,7 @@
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ghs_hYmZRBWP02iN1NdUi6FNudETTec6ho26dgiV"\
+  -H "Authorization: Bearer ghs_rHlwdaUN4fmNxdABPztYZjkwoUrJWI4E293p"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/QuanZhang-William/ah-test-catalog-git-versioning/check-runs \
-  -d '{"name":"tekton-ci","head_sha":"d737eff2f5963f15e6b18738bf449819d6537a74","status":"completed", "conclusion": "success", "started_at":"2023-05-04T01:14:52Z","output":{"title":"Mighty Readme report","summary":"","text":""}}'
+  https://api.github.com/repos/QuanZhang-William/my-golang-build/check-runs \
+  -d '{"name":"build-test","head_sha":"d314269603adf302b4b3b50d9f455b47517d9db5","status":"completed", "conclusion": "success", "started_at":"2023-05-04T01:14:52Z","output":{"title":"Mighty Readme report","summary":"","text":""}}'
